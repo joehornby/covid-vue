445dafc251d7db77efcd7d8110f3a3b778ec2223
Joseph Hornby <joehornby@gmail.com>
2020-11-03 12:56:08 +0000

    add local and api data fetch methods

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,15 @@ export default {
   components: {},
   data() {
     return {
-      covidCSV: '~assets/phe_cases_london_boroughs.csv',
-      covidData: []
+      covidApi: 'https://data.london.gov.uk/api/table/s8c9t_j4fs2?$limit=5000',
+      covidCsv: 'data/phe_cases_london_boroughs.csv',
+      covidData: null
     }
   },
   methods: {
-    async getLocalData(csv) {
-      const data = await d3.csv(csv).then((data) => {
+    getLocalData(csv) {
+      const parseDate = d3.timeParse('%Y-%m-%d')
+      const data = d3.csv(csv).then((data) => {
         data.forEach((d) => {
           // Convert strings to integers
           d.new_cases = +d.new_cases
@@ -27,13 +29,23 @@ export default {
           // Convert date string to date format
           d.date = parseDate(d.date)
         })
+        console.log(data)
       })
       this.covidData = data
     },
+    getData(api) {
+      this.$http.get(api).then((response) => {
+        this.covidData = response.data.rows
+        console.log(response.data.rows)
+      })
+    }
   },
   mounted() {
-    // Fetch data on mount
-    this.getLocalData(this.covidCSV)
+    /* Fetch data from API */
+    // this.getData(this.covidApi)
+
+    /* Fetch local data */
+    this.getLocalData(this.covidCsv)
   }
 }
 </script>
