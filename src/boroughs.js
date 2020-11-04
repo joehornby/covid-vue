1315864f85adb312d3e434ba8ff72820848dd4b1
Joseph Hornby <joehornby@gmail.com>
2020-11-04 15:08:03 +0000

    generate borough info object for map

---
+++ src/boroughs.js
@@ -0,0 +1,266 @@
+export const boroughInfo = [
+  {
+    "name" : "City of London",
+    "abbr" : "CIT",
+    "grid" : {
+      "row" : 3,
+      "col" : 4
+    }
+  },
+  {
+    "name" : "Barking and Dagenham",
+    "abbr" : "BARK",
+    "grid" : {
+      "row" : 3,
+      "col" : 7
+    }
+  },
+  {
+    "name" : "Barnet",
+    "abbr" : "BARN",
+    "grid" : {
+      "row" : 1,
+      "col" : 3
+    }
+  },
+  {
+    "name" : "Bexley",
+    "abbr" : "BEXL",
+    "grid" : {
+      "row" : 4,
+      "col" : 7
+    }
+  },
+  {
+    "name" : "Brent",
+    "abbr" : "BREN",
+    "grid" : {
+      "row" : 2,
+      "col" : 2
+    }
+  },
+  {
+    "name" : "Bromley",
+    "abbr" : "BROM",
+    "grid" : {
+      "row" : 5,
+      "col" : 5
+    }
+  },
+  {
+    "name" : "Camden",
+    "abbr" : "CAM",
+    "grid" : {
+      "row" : 2,
+      "col" : 3
+    }
+  },
+  {
+    "name" : "Croydon",
+    "abbr" : "CROY",
+    "grid" : {
+      "row" : 5,
+      "col" : 4
+    }
+  },
+  {
+    "name" : "Ealing",
+    "abbr" : "EAL",
+    "grid" : {
+      "row" : 2,
+      "col" : 1
+    }
+  },
+  {
+    "name" : "Enfield",
+    "abbr" : "ENF",
+    "grid" : {
+      "row" : 0,
+      "col" : 4
+    }
+  },
+  {
+    "name" : "Greenwich",
+    "abbr" : "GRE",
+    "grid" : {
+      "row" : 4,
+      "col" : 6
+    }
+  },
+  {
+    "name" : "Hackney",
+    "abbr" : "HAC",
+    "grid" : {
+      "row" : 2,
+      "col" : 5
+    }
+  },
+  {
+    "name" : "Hammersmith and Fulham",
+    "abbr" : "HAM",
+    "grid" : {
+      "row" : 3,
+      "col" : 1
+    }
+  },
+  {
+    "name" : "Haringey",
+    "abbr" : "HARI",
+    "grid" : {
+      "row" : 1,
+      "col" : 4
+    }
+  },
+  {
+    "name" : "Harrow",
+    "abbr" : "HARR",
+    "grid" : {
+      "row" : 1,
+      "col" : 2
+    }
+  },
+  {
+    "name" : "Havering",
+    "abbr" : "HAVE",
+    "grid" : {
+      "row" : 2,
+      "col" : 7
+    }
+  },
+  {
+    "name" : "Hillingdon",
+    "abbr" : "HILL",
+    "grid" : {
+      "row" : 2,
+      "col" : 0
+    }
+  },
+  {
+    "name" : "Hounslow",
+    "abbr" : "HOUN",
+    "grid" : {
+      "row" : 3,
+      "col" : 0
+    }
+  },
+  {
+    "name" : "Islington",
+    "abbr" : "ISL",
+    "grid" : {
+      "row" : 2,
+      "col" : 4
+    }
+  },
+  {
+    "name" : "Kensington and Chelsea",
+    "abbr" : "KEN",
+    "grid" : {
+      "row" : 3,
+      "col" : 2
+    }
+  },
+  {
+    "name" : "Kingston upon Thames",
+    "abbr" : "KING",
+    "grid" : {
+      "row" : 5,
+      "col" : 2
+    }
+  },
+  {
+    "name" : "Lambeth",
+    "abbr" : "LAM",
+    "grid" : {
+      "row" : 4,
+      "col" : 3
+    }
+  },
+  {
+    "name" : "Lewisham",
+    "abbr" : "LEW",
+    "grid" : {
+      "row" : 4,
+      "col" : 5
+    }
+  },
+  {
+    "name" : "Merton",
+    "abbr" : "MERT",
+    "grid" : {
+      "row" : 5,
+      "col" : 3
+    }
+  },
+  {
+    "name" : "Newham",
+    "abbr" : "NEWH",
+    "grid" : {
+      "row" : 3,
+      "col" : 6
+    }
+  },
+  {
+    "name" : "Redbridge",
+    "abbr" : "REDB",
+    "grid" : {
+      "row" : 2,
+      "col" : 6
+    }
+  },
+  {
+    "name" : "Richmond upon Thames",
+    "abbr" : "RICH",
+    "grid" : {
+      "row" : 4,
+      "col" : 1
+    }
+  },
+  {
+    "name" : "Southwark",
+    "abbr" : "SOU",
+    "grid" : {
+      "row" : 4,
+      "col" : 4
+    }
+  },
+  {
+    "name" : "Sutton",
+    "abbr" : "SUTT",
+    "grid" : {
+      "row" : 6,
+      "col" : 3
+    }
+  },
+  {
+    "name" : "Tower Hamlets",
+    "abbr" : "TOW",
+    "grid" : {
+      "row" : 3,
+      "col" : 5
+    }
+  },
+  {
+    "name" : "Waltham Forest",
+    "abbr" : "WALT",
+    "grid" : {
+      "row" : 1,
+      "col" : 5
+    }
+  },
+  {
+    "name" : "Wandsworth",
+    "abbr" : "WAN",
+    "grid" : {
+      "row" : 4,
+      "col" : 2
+    }
+  },
+  {
+    "name" : "Westminster",
+    "abbr" : "WES",
+    "grid" : {
+      "row" : 3,
+      "col" : 3
+    }
+  }
+]
