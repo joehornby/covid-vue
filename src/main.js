54a1b7f3e7ad785f574b9bea594c65c63e5a9879
Joseph Hornby <joehornby@gmail.com>
2020-11-05 13:05:36 +0000

    add three.js and borough map svg

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueStatic from 'vue-static'
 
 Vue.config.productionTip = false
 
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios, VueStatic)
 
 new Vue({
   render: h => h(App),
